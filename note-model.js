3a0a2c19720d87ddcd8943acab2aaeaa5b2ced96
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-03-06 10:32:03 +0000

    playing with semicolons

--- note-model.js
+++ note-model.js
@@ -1,11 +1,11 @@
 (function(exports) {
     function Note(newNote) {
         this.text = newNote;
-    };
+    }
 
     Note.prototype.returnNotes = function() {
-        return this.text
-    }
+        return this.text;
+    };
 
     exports.Note = Note;
 })(this);
